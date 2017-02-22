281c42b949b3f06cf86c38d9e840c8f72a219a63
ratona14 <floresalexx@gmail.com>
2017-02-22 01:59:22 -0800

    bootstraping

--- client/assets/factories/gameFactory.js
+++ client/assets/factories/gameFactory.js
@@ -5,10 +5,10 @@ app.factory('gameFactory', function($http){
 
 	var levelSelector = 1;
 
-	factory.getDefinition = function (word) {
-	let dictionaryUrl = 'http://www.dictionary.com/browse/' + word;
-	ctrl.url = dictionaryUrl;
-};
+	factory.getDefinition = function(word) {
+		let dictionaryUrl = 'http://www.dictionary.com/browse/' + word;
+		ctrl.url = dictionaryUrl;
+	};
 
 	factory.selectDifficulty = function(level) {
 		levelSelector = level;
